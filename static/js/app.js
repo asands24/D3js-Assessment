fa6af9b0e70e2de34e26158d1a23e4428f37d657
Alex Sands <asands44@gmail.com>
2021-06-01 14:08:54 -0400

    try on chrome

--- static/js/app.js
+++ static/js/app.js
@@ -23,28 +23,38 @@ var tooltip = d3.select('body')
                 .attr('opacity', '0');
 
 // Read and arrange the data
-
-
+d3.json("./data/data.json", function(data) {
+    console.log(data);
+});
 
 
 // Set scales
+const xScale = d3.scaleBand().rangeRound([0, 250]).padding(0.1);
+const yScale = d3.scaleLinear().domain([0, 100]).range([200, 0]);
 
-
+var scale = d3.scaleLinear()
+            .domain([0, 1000])
+            .range([50, 500]);
 
 
 // Create axes
+var x_axis = d3.axisBottom()
+                   .scale(scale);
 
 
 
 // Append the axes as G
 
 
-
-
-
 // Create Bars or Line function
-
-
+// const bars = container
+//   .selectAll('.bar')
+//   .data(data.json)
+//   .enter()
+//   .append('rect')
+//   .classed('bar', true)
+//   .attr('width', xScale.bandwidth())
+//   .attr('height', (data) => yScale(data.value));
 
 
 // Create Circle group
